186154ae670bb7bf592345b324e938bbfc7634ef
6210513029 <6210513029@psu.ac.th>
2021-10-03 19:26:44 +0700

    show_user

--- php_simple_CRUD/show_user.php
+++ php_simple_CRUD/show_user.php
@@ -1,11 +1,12 @@
 <?php
     session_start();
+
     //9.fetch and delete record
     include_once 'dbconnect.php';
 
     // fetch records
     $sql = "SELECT * FROM users ORDER BY user_id DESC"; //มากไปน้อย DESC น้อยไปมาก ASC
-    $rerult = mysqli_query($con, $sql);
+    $result = mysqli_query($con, $sql);
 
     $cnt = 1;
 
@@ -44,7 +45,7 @@
                  <!-- extra step, change menu after login -->
                  <?php if (isset($_SESSION['name'])) { ?>
                     <!-- true -->
-                    <li><p class = "navbar-text">Signed in as <?php echo $_SESSION['name']; ?></p></li>
+                    <li><p class = "navbar-text">Signed in as <?php echo $_SESSION['name'] ?></p></li>
                     <li><a href="logout.php">Log Out</a></li>
                  <?php } else { ?>
                     <!-- false -->
@@ -72,38 +73,33 @@
                          <th>Password</th>
                          <th colspan="2" style="text-align:center">Actions</th>
                      </tr>
-                 </thead>
-                 <tbody>
+                </thead>
+                <tbody>
                 <!--10.show all users in this part of table ใช้วน loop ด้วยคำสั่ง while -->
-                <?php while ($row = mysqli_fetch_array($rerult)) { ?>
+                <?php while ($row = mysqli_fetch_array($result)) { ?>
                     <tr>
                         <td><?php echo $cnt++; ?></td>
-                        <td><?php echo $row['user_name']?></td>
-                        <td><?php echo $row['user_email']?></td>
-                        <td><?php echo $row['user_passwd']?></td>
-                        <td>
-                            <input type="button" value="แก้ไข" name="btn-edit" class="btn btn-primary" onclick = "update_user (<?php echo $row['user_id']; ?>);">
-                        </td>
-                        <td>
-                            <input type="button" value="ลบ" name="btn-delete" class="btn btn-danger" onclick ="delete_user (<?php echo $row['user_id']; ?>);">
-                        </td>
+                        <td><?php echo $row['user_name'];?></td>
+                        <td><?php echo $row['user_email'];?></td>
+                        <td><?php echo $row['user_passwd'];?></td>
+                        <td><input type="button" value="แก้ไข" name="btn-edit" class="btn btn-primary" onclick = "update_user (<?php echo $row['user_id']; ?>);"></td>
+                        <td><input type="button" value="ลบ" name="btn-delete" class="btn btn-danger" onclick ="delete_user (<?php echo $row['user_id']; ?>);"></td>
                     </tr>
                 <?php } ?>
-
-                 </tbody>
+                </tbody>
              </table>
             </div>
             <!--12.display number of records -->
-            <div><?php echo mysqli_num_rows($rerult) . " rocord(s) found."; ?></div>
-
-         </div>
+            <div><?php echo mysqli_num_rows($result) . " rocord(s) found."; ?></div>
+        </div>
      </div>
  </div>
  <!--11.JavaScript for edit and delete actions -->
- <script>
+<script>
      //delete
      function delete_user(id) {
-        if (confirm("Are you sure to delete this record?")) {window.location.href = "show_user.php?user_id=" + id;
+        if (confirm("Are you sure to delete this record?")) {
+            window.location.href = "show_user.php?user_id=" + id;
         }
      }
      //update
@@ -111,6 +107,5 @@
          window.location.href = "update_user.php?user_id" + id;
      }
  </script>
-
  </body>
  </html>
